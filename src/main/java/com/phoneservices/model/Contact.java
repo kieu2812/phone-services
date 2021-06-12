9eed55be00a0ee00e33cbaa4287aa79554ea19a2
kieu2812 <kieu2812@gmail.com>
2021-06-12 11:33:06 -0400

    Init project

---
+++ src/main/java/com/phoneservices/model/Contact.java
@@ -0,0 +1,4 @@
+package com.phoneservices.model;
+
+public class Contact {
+}
---
+++ src/test/java/com/phoneservices/PhoneServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phoneservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
